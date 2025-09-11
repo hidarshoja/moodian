b5b806ce3c01d05692a3461539cb4b2c0c1a77dc
Heydar shoja <hidarshoja@gmail.com>
2025-09-11 23:11:36 +0330

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React  from "react";
 import { Menu } from "@headlessui/react";
 import { Bars3Icon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+import { MdOutlineFreeCancellation } from "react-icons/md";
+import { TbStatusChange } from "react-icons/tb";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { BiLogOutCircle } from "react-icons/bi";
+import { CiUser } from "react-icons/ci";
+import { TiPhoneOutline } from "react-icons/ti";
 
 export default function Header({
   setSidebarOpen,
   desktopSidebarOpen,
   setDesktopSidebarOpen,
 }) {
+  const [userMenuOpen, setUserMenuOpen] = React.useState(false);
+  const navigate = useNavigate();
+  const menuItems = [
+    { label: "ابطال", path: "/cancel" , icon :<MdOutlineFreeCancellation className="w-4 h-4" /> },
+    { label: "تغییر وضعیت", path: "/change-status" , icon :<TbStatusChange className="w-4 h-4 "/>},
+    { label: "تغییر رمز", path: "/change-password" , icon:<RiLockPasswordFill className="w-4 h-4 "/> },
+    { label: "خروج", path: "/logout" , icon :<BiLogOutCircle className="w-4 h-4 " />},
+  ];
+
   return (
     <div className="header-panel sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4  border-b-2 border-[#3b466c] px-4 sm:gap-x-6 sm:px-6 lg:px-8 shadow-sm text-right font-sans">
       <button
@@ -33,43 +49,59 @@ export default function Header({
             as="div"
             className="flex flex-wrap items-center justify-end gap-1"
           >
-            <div className="px-3 cursor-pointer flex overflow-hidden bg-white h-7 rounded-[11px] border border-[#5B7380] transition-all duration-300 hover:shadow-md">
-              <div className="w-full flex items-center justify-center gap-1">
-                <div>
-                  <img
-                    src="/img/zahra.svg"
-                    className="w-[20px] h-[20px] rounded-[20px] border border-[#000]"
-                    alt=""
-                  />
-                </div>
-                <div className="text-color3 text-sm md:text-[16px]">
-                  میلاد فاضلیان
-                </div>
+           
+            <div className=" flex overflow-hidden w-7 bg-white h-7 rounded-full border border-[#5B7380]">
+              <div className="w-full flex items-center justify-center gap-1 cursor-pointer  transition-colors duration-200">
+              <div onClick={() => setUserMenuOpen(!userMenuOpen)}>
+                   <FaRegCircleUser className="w-6 h-6 cursor-pointer" />
+                   </div>
+                   {userMenuOpen && (
+  <div
+    className="fixed top-10 left-0 w-[160px] h-full   z-50 flex flex-col items-start p-4 animate-slideIn"
+    style={{ animation: "slideIn 0.4s cubic-bezier(.68,-0.55,.27,1.55)" }}
+  >
+    <span className="w-[160px] border border-white bg-[#0d0d2f]  flex items-center justify-start gap-1 text-sm my-2  p-1 rounded-lg text-right text-white  hover:bg-[#1f1f53] transition-colors duration-200 opacity-0 animate-fadeInStagger">
+      <div className="flex flex-col">
+      <div className="flex items-center justify-start gap-1">
+      <CiUser />
+        <span>
+        حیدر شجاع 
+        </span>
+      </div>
+   
+      
+     
+      <div  className="flex items-center justify-start gap-1">
+      <TiPhoneOutline />
+        <span>
+        09376228320
+        </span>
+      </div>
+      </div>
+    
+     
+    </span>
+   {menuItems.map((item, idx) => (
+  <button
+    key={item.label}
+    onClick={() => navigate(item.path)}
+    className="w-[120px] mr-8 flex border border-white bg-[#0d0d2f] items-center justify-start gap-1 text-sm my-2  p-1 rounded-lg text-right text-white  hover:bg-[#1f1f53] transition-colors duration-200 opacity-0 animate-fadeInStagger"
+    style={{
+      animationDelay: `${0.35 * idx}s`, // فاصله بیشتر برای آرام‌تر شدن
+      animationFillMode: "forwards",
+    }}
+  >
+    <span>{item.icon}</span>  
+    <span>{item.label}</span>          
+    
+  </button>
+))}
+ 
+  </div>
+   )}
+         
               </div>
-            </div>
-            <div className="w-[166px] flex overflow-hidden bg-[#FDCB44] h-7 rounded-[11px] border border-[#5B7380]">
-              <div className="w-1/2 flex items-center justify-center gap-1 cursor-pointer hover:bg-black/10 transition-colors duration-200">
-                <div>
-                  <img
-                    src="/img/userW.svg"
-                    className="w-[17px] h-[17px]"
-                    alt=""
-                  />
-                </div>
-                <div className="text-white text-[12px]">
-                  <Link to="/auth/login">ورود</Link>
-                </div>
-              </div>
-              <div className="w-1/2 bg-[#5B7380] flex items-center justify-center gap-1 cursor-pointer border-r border-gray-500 hover:bg-black/10 transition-colors duration-200">
-                <div className="text-white text-[12px]">خروج</div>
-                <div>
-                  <img
-                    src="/img/exitW.svg"
-                    className="w-[17px] h-[17px]"
-                    alt=""
-                  />
-                </div>
-              </div>
+           
             </div>
           </Menu>
         </div>
