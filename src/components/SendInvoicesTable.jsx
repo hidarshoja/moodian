43dd1a87ca2d70ab014ac069fbb75ea46137ca88
Heydar shoja <hidarshoja@gmail.com>
2025-09-21 10:00:47 +0330

    edit page invoice

--- src/components/SendInvoicesTable.jsx
+++ src/components/SendInvoicesTable.jsx
@@ -1,9 +1,95 @@
 import Spinner from "../utils/Spinner";
+import { formatDateToYMD   } from "../utils/change-date";
+import { FiTrash2, FiEdit2 } from "react-icons/fi";
+import axiosClient from "../axios-client";
+import Swal from 'sweetalert2';
 
 export default function SendInvoicesTable({
   records,
   loading
 }) {
+
+    const handleDelete = async (row) => {
+    try {
+      const res = await axiosClient.delete(`/invoices/${row.id}`);
+      console.log(`Delete response:`, res);
+
+    //  setRefresh(!refresh);
+      Swal.fire({
+        toast: true,
+        position: 'top-start', 
+        icon: 'success', // یا 'error'
+        title: 'محصول با موفقیت حذف شد',
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        customClass: {
+          popup: 'swal2-toast'
+        }
+      });
+  
+    
+    } catch (error) {
+      console.log(`error`, error);
+      Swal.fire({
+        toast: true,
+        position: 'top-start',
+        icon: 'error',
+        title: 'خطا در حذف محصول',
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        customClass: {
+          popup: 'swal2-toast'
+        }
+      });
+    }
+   
+   
+  };
+
+
+
+  const handleEdit = async (row) => {
+   
+      try {
+      const payload = { id: row.id, ...row };
+      const { data } = await axiosClient.put(
+        `/invoices/${payload.id}`,
+        payload
+      );
+   //   setRefresh(!refresh);
+      Swal.fire({
+        toast: true,
+        position: 'top-start', 
+        icon: 'success', // یا 'error'
+        title: 'محصول با موفقیت ویرایش شد',
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        customClass: {
+          popup: 'swal2-toast'
+        }
+      });
+   
+    } catch (error) {
+      console.log(`error`, error);
+      Swal.fire({
+        toast: true,
+        position: 'top-start',
+        icon: 'error',
+        title: 'خطا در ویرایش محصول',
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: true,
+        customClass: {
+          popup: 'swal2-toast'
+        }
+      });
+    }
+   
+  };
+
   return (
     <div className="overflow-x-auto nice-scrollbar rounded-2xl border border-white/10 bg-white/5 relative">
        {loading && (
@@ -56,10 +142,10 @@ export default function SendInvoicesTable({
                 {r.inp === 1 ? r.ins_label : "-"}
               </td>
               <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-              {r.inp === 2 ? r.ins_label : "-"}
+              {r?.customer?.name}{" "}{r?.customer?.last_name}
               </td>
               <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-              {r.inp === 3 ? r.ins_label : "-"}
+              { formatDateToYMD(r.created_at)}
               </td>
               <td className="px-4 py-3 text-white/90 text-sm truncate max-w-[200px]">
               {r.inp === 4 ? r.ins_label : "-"}
@@ -69,7 +155,21 @@ export default function SendInvoicesTable({
               </td>
               
             <td className="px-4 py-3 text-white/90 text-sm truncate max-w-[240px]">
-                {r.asn}
+                   <div className="flex items-center justify-center gap-2">
+                <button
+                  className="p-1 rounded hover:bg-red-500/20 text-red-500"
+                  onClick={() => handleDelete(r)}
+                 
+                >
+                  <FiTrash2 className="w-4 h-4" />
+                </button>
+                <button
+                  className="p-1 rounded hover:bg-blue-500/20 text-blue-500"
+                  onClick={() => handleEdit(r)}
+                >
+                  <FiEdit2 className="w-4 h-4" />
+                </button>
+              </div>
               </td>
             </tr>
           ))}
