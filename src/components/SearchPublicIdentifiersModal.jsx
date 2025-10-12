f08e6f4f134bcdc9932aeecb98d8a074321f9ffe
Heydar shoja <hidarshoja@gmail.com>
2025-10-12 20:06:51 +0330

    edit page products

--- src/components/SearchPublicIdentifiersModal.jsx
+++ src/components/SearchPublicIdentifiersModal.jsx
@@ -50,7 +50,7 @@ export default function SearchPublicIdentifiersModal({
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur animate-fadeInStagger"
+      className="fixed inset-0 z-50 flex items-center justify-center  backdrop-blur animate-fadeInStagger"
       onClick={onClose}
     >
       <div
