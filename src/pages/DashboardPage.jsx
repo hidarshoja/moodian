62808e0fb9dd0ec08590c3bbeff925073a8e20d3
Heydar shoja <hidarshoja@gmail.com>
2025-11-01 15:28:25 +0330

    edit again

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -90,7 +90,7 @@ console.log(`activityUser`, activityUser);
           </div>
         </div>
         <div className="w-full flex items-center justify-center text-xl text-white mt-10">
-          کاربر { parsedUser.name + " " + parsedUser.last_name }  به پنل مدیریت خود خوش آمدید!
+          کاربر { parsedUser?.name + " " + parsedUser?.last_name }  به پنل مدیریت خود خوش آمدید!
         </div>
         
            <div className="p-6">
