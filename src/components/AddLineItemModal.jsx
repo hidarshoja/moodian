dd32f5e742f4880f62bd91ef74671dfa1a0d2e1f
Heydar shoja <hidarshoja@gmail.com>
2025-11-16 09:46:59 +0330

    EDIT IN INVOICE

--- src/components/AddLineItemModal.jsx
+++ src/components/AddLineItemModal.jsx
@@ -10,8 +10,8 @@ export default function AddLineItemModal({
   onSave,
   initialData,
   title,
-  selectedProduct,
-  setSelectedProduct,
+  selectedProduct = null,
+  setSelectedProduct = () => {},
 }) {
   const [formData, setFormData] = useState({
     ProductId: "",
