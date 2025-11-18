de0e3be90d572add16aa97424ef170838b17a1a0
Heydar shoja <hidarshoja@gmail.com>
2025-11-18 15:55:04 +0330

    edit modal

--- src/components/AssignModalNew.jsx
+++ src/components/AssignModalNew.jsx
@@ -1,9 +1,9 @@
-import  { useState , useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Pagination from "./Pagination";
 import PropTypes from "prop-types";
 import axiosClient from "../axios-client";
 import Swal from "sweetalert2";
-import {convertToPersianDate} from "../utils/change-date";
+import { convertToPersianDate } from "../utils/change-date";
 
 function Spinner() {
   return (
@@ -13,12 +13,28 @@ function Spinner() {
   );
 }
 
-export default function AssignModalNew({ transaction , onClose , loading , meta , setPageCount , pageCount , setLoading , idActive , activeAccount, refresh , setRefresh, loading3 , setPageCount2 , pageCount2 , setLoading3}) {
+export default function AssignModalNew({
+  transaction,
+  onClose,
+  loading,
+  meta,
+  setPageCount,
+  pageCount,
+  setLoading,
+  idActive,
+  activeAccount,
+  refresh,
+  setRefresh,
+  loading3,
+  setPageCount2,
+  pageCount2,
+  setLoading3,
+}) {
   const [selectedTransactions, setSelectedTransactions] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     if (activeAccount && activeAccount.length > 0) {
-      const activeIds = activeAccount.map(acc => acc.id);
+      const activeIds = activeAccount.map((acc) => acc.id);
       setSelectedTransactions(activeIds);
     }
   }, [activeAccount]);
@@ -34,172 +50,181 @@ export default function AssignModalNew({ transaction , onClose , loading , meta 
   };
 
   const handleShowAssign = () => {
-     const invoices = selectedTransactions;
-axiosClient.post(`/transactions/${idActive}/assign-invoices` , {invoices})
-.then((response) => {
-  console.log(`response`, response);
-    setRefresh(!refresh);
-  Swal.fire({
-    icon: 'success',
-    title: 'موفقیت آمیز',
-    text: 'عملیات با موفقیت انجام شد',
-  })
-  onClose();
-    })
+    const invoices = selectedTransactions;
+    axiosClient
+      .post(`/transactions/${idActive}/assign-invoices`, { invoices })
+      .then((response) => {
+        console.log(`response`, response);
+        setRefresh(!refresh);
+        Swal.fire({
+          icon: "success",
+          title: "موفقیت آمیز",
+          text: "عملیات با موفقیت انجام شد",
+        });
+        onClose();
+      })
       .catch((err) => {
         console.error("Error assigning transactions:", err);
         Swal.fire({
-          icon: 'error',
-          title: 'خطا',
-          text: 'عملیات با خطا مواجه شد',
-        })
+          icon: "error",
+          title: "خطا",
+          text: "عملیات با خطا مواجه شد",
+        });
       });
-
   };
-console.log(`transaction`, transaction);
+  console.log(`transaction`, transaction);
   return (
-    <div
-         className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur animate-fadeInStagger"
-       >
-         <div
-           className="w-full max-w-5xl flex flex-col rounded-2xl bg-[#23234a] border border-white/10 shadow-2xl relative animate-slideIn"
-           onClick={(e) => e.stopPropagation()}
-         >
-              {/* Header */}
-           <div className="flex items-center justify-between px-6 py-4 border-b border-white/10 bg-[#0a0a22] rounded-t-2xl">
-             <div className="flex items-center gap-3">
-             
-               <span className="text-white text-lg font-bold">اساین 2فاکتور</span>
-             </div>
-             <button
-               onClick={onClose}
-               className="text-white/80 hover:text-white transition-colors"
-             >
-              X
-             </button>
-           </div>
-          <div className="overflow-x-auto nice-scrollbar rounded-2xl border border-white/10 bg-white/5 p-3  relative">
-                 {loading3 && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-white/60 z-20">
-                    <Spinner />
-                  </div>
-                )}
-                <table 
-                  className={`min-w-full text-white ${
-                    loading3 ? "opacity-30 pointer-events-none" : ""
-                  }`}
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur animate-fadeInStagger">
+      <div
+        className="w-full max-w-5xl flex flex-col rounded-2xl bg-[#23234a] border border-white/10 shadow-2xl relative animate-slideIn"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* Header */}
+        <div className="flex items-center justify-between px-6 py-4 border-b border-white/10 bg-[#0a0a22] rounded-t-2xl">
+          <div className="flex items-center gap-3">
+            <span className="text-white text-lg font-bold">اساین 2فاکتور</span>
+          </div>
+          <button
+            onClick={onClose}
+            className="text-white/80 hover:text-white transition-colors"
+          >
+            X
+          </button>
+        </div>
+        <div className="overflow-x-auto nice-scrollbar rounded-2xl border border-white/10 bg-white/5 p-3  relative">
+          {loading3 && (
+            <div className="absolute inset-0 flex items-center justify-center bg-white/60 z-20">
+              <Spinner />
+            </div>
+          )}
+          <table
+            className={`min-w-full text-white ${
+              loading3 ? "opacity-30 pointer-events-none" : ""
+            }`}
+          >
+            <thead>
+              <tr className="text-white/80 text-sm bg-[#181f3a]">
+                <th className="text-right px-4 py-3 whitespace-nowrap"></th>
+                <th className="text-right px-4 py-3 whitespace-nowrap">#</th>
+                <th className="text-right px-4 py-3 whitespace-nowrap">
+                  تاریخ صدور{" "}
+                </th>
+                <th className="text-right px-4 py-3 whitespace-nowrap">
+                  نام مشتری{" "}
+                </th>
+                <th className="text-right px-4 py-3 whitespace-nowrap">
+                  {" "}
+                  txid{" "}
+                </th>
+                <th className="text-center px-4 py-3 whitespace-nowrap">
+                  وضعیت
+                </th>
+                <th className="text-center px-4 py-3 whitespace-nowrap">
+                  مبلغ کل{" "}
+                </th>
+                <th className="text-center px-4 py-3 whitespace-nowrap">
+                  {" "}
+                  مبلغ باقیمانده{" "}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {(!transaction || transaction.length === 0) && (
+                <tr>
+                  <td
+                    colSpan={4}
+                    className="px-4 py-6 text-center text-white/60 text-sm"
+                  >
+                    موردی ثبت نشده است.
+                  </td>
+                </tr>
+              )}
+              {transaction.map((r, i) => (
+                <tr
+                  key={r.id ?? i}
+                  className="odd:bg-white/5 even:bg-white/10 border-t border-white/5"
                 >
-                  <thead>
-                    <tr className="text-white/80 text-sm bg-[#181f3a]">
-                    <th className="text-right px-4 py-3 whitespace-nowrap"></th>
-                      <th className="text-right px-4 py-3 whitespace-nowrap">#</th>
-                      <th className="text-right px-4 py-3 whitespace-nowrap">تاریخ صدور </th>
-                      <th className="text-right px-4 py-3 whitespace-nowrap">نام مشتری </th>
-                      <th className="text-right px-4 py-3 whitespace-nowrap">  txid   </th>
-                        <th className="text-center px-4 py-3 whitespace-nowrap">وضعیت</th>
-                          <th className="text-center px-4 py-3 whitespace-nowrap">مبلغ کل  </th>
-                          <th className="text-center px-4 py-3 whitespace-nowrap"> مبلغ باقیمانده  </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {(!transaction || transaction.length === 0) && (
-                      <tr>
-                        <td
-                          colSpan={4}
-                          className="px-4 py-6 text-center text-white/60 text-sm"
-                        >
-                          موردی ثبت نشده است.
-                        </td>
-                      </tr>
-                    )}
-                    {transaction.map((r, i) => (
-                      <tr
-                        key={r.id ?? i}
-                        className="odd:bg-white/5 even:bg-white/10 border-t border-white/5"
-                      >
-                           <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                           <input
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    <input
                       type="checkbox"
                       checked={selectedTransactions.includes(r.id)}
                       onChange={() => handleCheckboxChange(r.id)}
                     />
-                        </td>
-                        <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                          {r?.id}
-                        </td>
-                        <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                          {convertToPersianDate(r?.indatim)}
-                        </td>
-                        <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                          {r?.customer?.name} - {r?.customer?.last_name}
-                        </td>
-                        <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                          {r?.taxid}
-                        </td>
-                         <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                          {r?.status_label}
-                        </td>
-                         <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                          {new Intl.NumberFormat('fa-IR').format(r?.tadis)}
-                        </td>
-                        <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
-                        {r.amount && new Intl.NumberFormat('fa-IR').format(r?.amount - r?.sum_invoices_assigned_amount) }
-                        {r.tadis && new Intl.NumberFormat('fa-IR').format(r?.tadis - r?.sum_transactions_assigned_amount) }
-                        </td>
-                        
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-             <div className='w-full flex items-center justify-end'>
-               <div className="flex items-center  gap-2 w-full">
-                <div className='w-2/5'></div>
-                 <div className='w-1/5 flex items-center justify-center '>
-                             <Pagination
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {r?.id}
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {convertToPersianDate(r?.indatim)}
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {r?.customer?.name} - {r?.customer?.last_name}
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {r?.taxid}
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {r?.status_label}
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {new Intl.NumberFormat("fa-IR").format(r?.tadis)}
+                  </td>
+                  <td className="px-4 py-3 text-white/90 text-sm whitespace-nowrap">
+                    {r.amount
+                      ? new Intl.NumberFormat("fa-IR").format(
+                          r?.amount - r?.sum_invoices_assigned_amount
+                        )
+                      : r.tadis &&
+                        new Intl.NumberFormat("fa-IR").format(
+                          r?.tadis - r?.sum_transactions_assigned_amount
+                        )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="w-full flex items-center justify-end">
+            <div className="flex items-center  gap-2 w-full">
+              <div className="w-2/5"></div>
+              <div className="w-1/5 flex items-center justify-center ">
+                <Pagination
                   meta={meta}
                   pageCount={pageCount2}
                   setPageCount={setPageCount2}
                   setLoading={setLoading3}
                 />
-                 </div>
-                 <div className='w-1/5'></div>
-                 <div className='w-1/5 flex items-center justify-end'>
-                  
-                            <button
-                             onClick={handleShowAssign}
-                             
-                              className="p-2 rounded-lg bg-green-500/10 text-green-400 border border-red-500/20 hover:bg-red-500/15"
-                            >
-                             ذخیره کردن 
-                            </button>
-                 </div>
-                          </div>
-                
-             </div>
               </div>
-           </div>
-         
-   </div>
-
-    
+              <div className="w-1/5"></div>
+              <div className="w-1/5 flex items-center justify-end">
+                <button
+                  onClick={handleShowAssign}
+                  className="p-2 rounded-lg bg-green-500/10 text-green-400 border border-red-500/20 hover:bg-red-500/15"
+                >
+                  ذخیره کردن
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-
 AssignModalNew.propTypes = {
-  transaction : PropTypes.array,
-  loading3 : PropTypes.bool,
-  loading : PropTypes.bool,
-  onClose:PropTypes.func,
-  meta:PropTypes.object,
-  pageCount:PropTypes.number,
-  setPageCount:PropTypes.func,
-  pageCount2:PropTypes.number,
-  setPageCount2:PropTypes.func,
-  setLoading:PropTypes.func,
-  setLoading3:PropTypes.func,
-  idActive:PropTypes.number,
-  activeAccount:PropTypes.array,
-   refresh:PropTypes.bool,
-   setRefresh:PropTypes.func,
+  transaction: PropTypes.array,
+  loading3: PropTypes.bool,
+  loading: PropTypes.bool,
+  onClose: PropTypes.func,
+  meta: PropTypes.object,
+  pageCount: PropTypes.number,
+  setPageCount: PropTypes.func,
+  pageCount2: PropTypes.number,
+  setPageCount2: PropTypes.func,
+  setLoading: PropTypes.func,
+  setLoading3: PropTypes.func,
+  idActive: PropTypes.number,
+  activeAccount: PropTypes.array,
+  refresh: PropTypes.bool,
+  setRefresh: PropTypes.func,
 };
