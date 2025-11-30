a9a07b6571856432c7af05e8e0a8ce91e492404c
Heydar shoja <hidarshoja@gmail.com>
2025-11-30 12:02:53 +0330

    edit page comparel

---
+++ src/components/FactorInvoiceCompare.jsx
@@ -0,0 +1,259 @@
+
+import  { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {convertToPersianDate} from "../utils/change-date";
+import { IoMdAlert } from "react-icons/io";
+import { IoMdCheckmarkCircle } from "react-icons/io";
+import { IoCloseCircle } from "react-icons/io5";
+import TransactionModal from './TransactionModal';
+import AssignModal from "./AssignModal";
+import axiosClient from "../axios-client";
+import { MdAssignmentAdd } from "react-icons/md";
+import { FaReceipt } from "react-icons/fa";
+import Pagination from "./Pagination";
+
+function Spinner() {
+  return (
+    <div className="flex justify-center items-center w-full h-60">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-400"></div>
+    </div>
+  );
+}
+
+
+export default function FactorInvoiceCompare({invoiceData, loading , refresh , setRefresh , setLoading , meta3 , pageCount3 ,setPageCount3}) {
+  const [transactionModalOpen, setTransactionModalOpen] = useState(false);
+  const [assignModalOpen, setAssignModalOpen] = useState(false);
+  const [transactionData, setTransactionData] = useState([]);
+   const [assignData, setAssignData] = useState([]);
+   const[loading2 , setLoading2] = useState(false);
+   const[loading3 , setLoading3] = useState(false);
+const[idActive , setIdActive] = useState(null);
+     const [meta, setMeta] = useState({});
+const[activeAccount , setActiveAccount] = useState([]);
+const[pageCount2 , setPageCount2] = useState(1);
+  const handleShowTransaction = (i, r) => {
+    setTransactionModalOpen(true);
+    setLoading2(true);
+axiosClient.get(`/invoices/${r.id}`).then((response) => {
+      setTransactionData(response?.data?.transactions);
+    }).catch((error) => {
+      console.log(error);
+    }).finally(() => {
+      setLoading2(false);
+    })
+
+  };
+
+   const handleShowAssign = (i, r) => {
+    if(r){
+      setIdActive(r.id);
+    }
+    setAssignModalOpen(true);
+   setLoading3(true);
+axiosClient.get(`/transactions?page=${pageCount2}&f[coefficient]=1&f[sum_invoices] = <,amount,${r.id}`)
+.then((response) => {
+      setAssignData(response?.data?.data);
+       setMeta(response.data.meta);
+    }).catch((error) => {
+      console.log(error);
+    }).finally(() => {
+      setLoading3(false);
+    })
+
+  };
+
+  useEffect(() => {
+    if(idActive){
+      handleShowAssign();
+    }
+  }, [pageCount2]);
+
+  const handleResponse = () => {
+    // axiosClient.get(`/transactions`).then((response) => {
+    //   setAssignData(response?.data?.data);
+    //    setMeta(response.data.meta);
+    // });
+  };
+
+  const handleCloseTransactionModal = () => {
+    setTransactionModalOpen(false);
+  };
+
+  const handleCloseAssignModal = () => {
+    setAssignModalOpen(false);
+  };
+  
+useEffect(() => {
+  if(idActive){
+    axiosClient.get(`/invoices/${idActive}`)
+    .then((response) => {
+      setActiveAccount(response?.data?.transactions);
+    });   
+  }else{
+    handleResponse();
+  }
+  }, [idActive , refresh]);
+ 
+
+  return (
+    <div>
+      <h1 className="text-white text-2xl font-bold mt-6">تعیین تکلیف شده ها</h1>
+    <div className="overflow-x-auto nice-scrollbar rounded-2xl border border-white/10 bg-white/5 mt-6 relative">
+        {loading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white/60 z-20">
+          <Spinner />
+        </div>
+      )}
+      <table 
+       className={`min-w-full text-white ${
+          loading ? "opacity-30 pointer-events-none" : ""
+        }`}
+      >
+        <thead>
+          <tr className="text-white/80 text-sm bg-[#181f3a]">
+          <th className="text-right px-4 py-3 whitespace-nowrap"></th>
+            <th className="text-right px-4 py-3 whitespace-nowrap">#</th>
+            <th className="text-right px-4 py-3 whitespace-nowrap">تاریخ صدور</th>
+            <th className="text-right px-4 py-3 whitespace-nowrap">نام مشتری</th>
+            <th className="text-right px-4 py-3 whitespace-nowrap">شماره فاکتور مودیان</th>
+              <th className="text-center px-4 py-3 whitespace-nowrap">وضعیت</th>
+                 <th className="text-center px-4 py-3 whitespace-nowrap">مبلغ </th>
+                    <th className="text-center px-4 py-3 whitespace-nowrap">مجموع تراکنش ها </th>
+                <th className="text-center px-4 py-3 whitespace-nowrap">دارای تراکنش</th>
+                <th className="text-right px-4 py-3 whitespace-nowrap"></th>
+                <th className="text-center px-2 py-3 whitespace-nowrap border-r border-white/10 relative"
+               style={{
+                position: "sticky",
+                left: 0,
+                backgroundColor: "#181f3a",
+                zIndex: 10,
+                minWidth: "70px",
+                boxShadow: "10px 0 20px rgba(24, 31, 58, 1)",
+              }}>عملیات</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(!invoiceData || invoiceData.length === 0) && (
+            <tr>
+              <td
+                colSpan={8}
+                className="px-4 py-6 text-center text-white/60 text-sm"
+              >
+                موردی ثبت نشده است.
+              </td>
+            </tr>
+          )}
+          {invoiceData.map((r, i) => (
+            <tr
+              key={r.id ?? i}
+              className="odd:bg-white/5 even:bg-white/10 border-t border-white/5"
+            >
+              <td className="p-2 text-center"></td>
+              <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+                {r?.id}
+              </td>
+              <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+                {convertToPersianDate(r?.created_at)}
+              </td>
+              <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+                {r?.customer?.name} -  {r?.customer?.last_name}
+              </td>
+              <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+                {r?.taxid}
+              </td>
+               <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+                {r?.status_label}
+              </td>
+               <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+             
+                  {Number(r?.tadis).toLocaleString()}
+              </td>
+               <td className="px-4 text-center py-3 text-white/90 text-sm whitespace-nowrap">
+                {Number(r?.sum_transactions_assigned_amount).toLocaleString()}
+              </td>
+               <td className="px-4 flex items-center justify-center py-3 text-white/90 text-sm whitespace-nowrap">
+               
+                {Number(r?.sum_transactions_assigned_amount) == 0 ? <IoCloseCircle className="text-red-500 w-5 h-5"/> : ""}
+                {r?.tadis  <= Number(r?.sum_transactions_assigned_amount) ?
+                 <IoMdCheckmarkCircle className="text-green-500 w-5 h-5"/> :(Number(r?.sum_transactions_assigned_amount) > 0  ? <IoMdAlert className="text-yellow-500 w-5 h-5"/> : "")
+                  }
+                {/* {Number(r?.transactions_sum_amount) > 0  ? <IoMdAlert className="text-yellow-500 w-5 h-5"/> : ""} */}
+              </td>
+              <td className="p-2  text-center"></td>
+                <td 
+               className="px-2 py-3 text-sm border-r border-white/5 relative flex items-center justify-center min-w-[60px] md:min-w-[160px]"
+                 style={{
+                   position: "sticky",
+                   left: 0,
+                   zIndex: 10,
+                   backgroundColor: "rgb(27, 33, 60)",
+                   boxShadow: "10px 0 20px rgba(0, 0, 0, 0.5)",
+                 }}>
+                <div className="flex items-center justify-center gap-2">
+                  <button
+                    onClick={() => handleShowTransaction(i, r)}
+                     className="p-2 rounded-lg bg-blue-500/10 text-blue-500 border border-blue-500/20 hover:bg-blue-500/15 transition-colors"
+                  >
+               <FaReceipt />
+                  </button>
+                  <button
+                   onClick={() => handleShowAssign?.(i , r)}
+                    className="p-2 rounded-lg bg-green-500/10 text-green-500 border border-green-500/20 hover:bg-green-500/15"
+                  >
+                   <MdAssignmentAdd />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    
+       {transactionModalOpen && (
+        <TransactionModal
+         transaction={transactionData}
+          onClose={handleCloseTransactionModal} 
+          loading={loading2}
+           />
+      )}
+       {assignModalOpen && (
+        <AssignModal
+         transaction={assignData}
+        meta={meta}
+        onClose={handleCloseAssignModal}
+        idActive={idActive}
+        activeAccount={activeAccount}
+        refresh={refresh}
+        setRefresh={setRefresh}
+        setPageCount2={setPageCount2}
+        pageCount2={pageCount2}
+        loading3={loading3}
+        />
+      )}
+    </div>
+      <div className="w-full flex items-center justify-center">
+      <Pagination
+        meta={meta3}
+        pageCount={pageCount3}
+        setPageCount={setPageCount3}
+        setLoading={setLoading}
+      />
+      </div>
+    </div>
+  )
+}
+
+
+FactorInvoiceCompare.propTypes = {
+  invoiceData: PropTypes.array,
+onShow: PropTypes.func,
+onAssign: PropTypes.func,
+loading :PropTypes.bool,
+refresh :PropTypes.bool,
+setRefresh:PropTypes.func,
+setLoading:PropTypes.func,
+meta3:PropTypes.object,
+pageCount3:PropTypes.number,
+setPageCount3:PropTypes.func
+};
--- src/pages/CompareInvoices.jsx
+++ src/pages/CompareInvoices.jsx
@@ -1,201 +1,178 @@
 
-import { useState, useEffect, useCallback } from "react";
-import ReportsFilterBill from "../components/ReportsFilterBill";
-import BillRecordsTable from "../components/BillRecordsTable";
+import { useState , useEffect } from "react";
 import axiosClient from "../axios-client";
-import Pagination from "../components/Pagination";
-import InvoiceDetailsModal from "../components/InvoiceDetailsModal";
+import InvoiceAccountTable from "../components/InvoiceAccountCompare";
+import AccountInvois from "../components/AccountInvoisCompare";
+import AccountInvoisAll from "../components/AccountInvoisAllCompare";
+import FactorInvoice from "../components/FactorInvoiceCompare";
 
 export default function CompareInvoices() {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [pageCount, setPageCount] = useState(1);
-  const [loading, setLoading] = useState(true);
-  const [dataTable, setDataTable] = useState([]);
-  const [filteredData, setFilteredData] = useState([]); // جدید
-  const [meta, setMeta] = useState({});
-  const [searchTerm, setSearchTerm] = useState("");
-  const [status, setStatus] = useState([]);
-  const [fromMonth, setFromMonth] = useState(null);
-  const [toMonth, setToMonth] = useState(null);
-  const [selectedCustomerId, setSelectedCustomerId] = useState(null);
-  const [isInvoiceDetailsOpen, setIsInvoiceDetailsOpen] = useState(false);
-  const [transactionType, setTransactionType] = useState("all"); // همه، واریز، برداشت
-  const [trackingCode, setTrackingCode] = useState(""); // کد رهگیری
-
-  const [filterRemove, setFilterRemove] = useState(true);
-  const [activeFilters, setActiveFilters] = useState({});
-
-  const buildFilterQuery = useCallback(
-    (filters) => {
-      const params = [];
-      Object.entries(filters).forEach(([key, value]) => {
-        if (value) {
-          if (key === "type") {
-            params.push(`${key}=${value}`);
-          } else if (key.startsWith("f[date]") && filterRemove) {
-            params.push(`${key}=${encodeURIComponent(value)}`);
-          } else if (key === "coefficient" && filterRemove) {
-            params.push(`f[coefficient]=${value}`);
-          } else if (key === "tracking_code" && filterRemove) {
-            params.push(`f[tracking_code]=${encodeURIComponent(value)}`);
-          } else if (filterRemove) {
-            const encodedValue =
-              key === "status" ? value : encodeURIComponent(value);
-            params.push(`f[${key}]=${encodedValue}`);
-          }
-        }
-      });
-      return params.length ? "&" + params.join("&") : "";
-    },
-    [filterRemove]
-  );
-
-  useEffect(() => {
-    setLoading(true);
-    let query = buildFilterQuery(activeFilters);
-    axiosClient
-      .get(`/transactions?page=${pageCount}${query}`)
-      .then((response) => {
-        setDataTable(response.data.data || []);
-        setMeta(response.data?.meta || {});
-      })
-      .catch((error) => {
+ const [activeBtn , setActiveBtn] = useState("invoiceAccount");
+const [invoiceData , setInvoiceData] = useState([]);
+const [transactionData2 , setTransactionData2] = useState([]);
+const [transactionDataAll , setTransactionDataAll] = useState([]);
+const [meta2, setMeta2] = useState({});
+const [pageCount, setPageCount] = useState(1);
+const [loading, setLoading] = useState(true);
+const[refresh , setRefresh] = useState(false);
+const [meta3, setMeta3] = useState({});
+const [pageCount3, setPageCount3] = useState(1);
+const [factorData, setFactorData] = useState([]);
+// فاکتور با حساب
+   useEffect(() => {
+     setLoading(true);
+     axiosClient.get(`/invoices?page=${pageCount3}&f[sum_transactions] = <,tadis`)
+     .then((response) => {
+      setInvoiceData(response.data.data);
+       setMeta3(response.data.meta);
+    })
+    .catch((error) => {
         console.error("Error fetching data:", error);
       })
       .finally(() => setLoading(false));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeFilters, pageCount, filterRemove]);
+  }, [refresh,pageCount3]);
 
-  // فیلتر کردن داده‌ها بر اساس searchTerm
+  //  حساب با فاکتور
+ 
+   useEffect(() => {
+     setLoading(true);
+     axiosClient.get(`/transactions?page=${pageCount}&f[coefficient]=1&f[sum_invoices] = <,amount`)
+     .then((response) => {
+      setTransactionData2(response.data.data);
+        setMeta2(response.data.meta);
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => setLoading(false));
+   
+  }, [pageCount,refresh]);
+  
+// تعیین تکلیف شده حساب
+   useEffect(() => {
+     setLoading(true);
+     axiosClient.get(`/transactions?page=${pageCount}&f[coefficient]=1&f[sum_invoices] = >=,amount`)
+     .then((response) => {
+      setTransactionDataAll(response.data.data);
+        setMeta2(response.data.meta);
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+      })
+      .finally(() => setLoading(false));
+   
+  }, [pageCount,refresh]);
+
+// تعیین تکلیف شده فاکتور
   useEffect(() => {
-    if (!Array.isArray(dataTable)) {
-      setFilteredData([]);
-      return;
-    }
-    if (searchTerm.trim() === "") {
-      setFilteredData(dataTable);
-    } else {
-      const filtered = dataTable.filter(
-        (item) => item.title && item.title.includes(searchTerm.trim())
-      );
-      setFilteredData(filtered);
-    }
-  }, [searchTerm, dataTable]);
+    setLoading(true);
+    axiosClient.get(`/invoices?page=${pageCount3}&f[sum_transactions] = >=,tadis`)
+    .then((response) => {
+     setFactorData(response.data.data);
+     setMeta3(response.data.meta);
+   })
+   .catch((error) => {
+       console.error("Error fetching data:", error);
+     })
+     .finally(() => setLoading(false));
+  
+ }, [pageCount,refresh]);
 
-  const handleStartDateChange = (selectedDate) => {
-    setStartDate(selectedDate);
-  };
+   
 
-  const handleEndDateChange = (selectedDate) => {
-    setEndDate(selectedDate);
-  };
-
-  const handleClearAll = () => {
-    setStartDate(null);
-    setEndDate(null);
-    setFilterRemove(false);
-    setStatus([]);
-    setFromMonth(null);
-    setToMonth(null);
-    setTransactionType("all");
-    setTrackingCode("");
-  };
-
-  function toEnglishDigits(str) {
-    if (!str) return "";
-    return str.replace(/[۰-۹]/g, (d) => "0123456789"["۰۱۲۳۴۵۶۷۸۹".indexOf(d)]);
-  }
-
-  const handleSendAll = () => {
-    setFilterRemove(true);
-    const start = toEnglishDigits(startDate?.format?.("YYYY/MM/DD") || "");
-    const end = toEnglishDigits(endDate?.format?.("YYYY/MM/DD") || "");
-    const startMonth = toEnglishDigits(fromMonth?.format?.("YYYY/MM/DD") || "");
-    const endMonth = toEnglishDigits(toMonth?.format?.("YYYY/MM/DD") || "");
-
-    let minDate = "";
-    let maxDate = "";
-
-    if (start && end) {
-      minDate = start;
-      maxDate = end;
-    } else if (startMonth && endMonth) {
-      minDate = startMonth;
-      maxDate = endMonth;
-    }
-
-    const newFilters = {
-      "f[date][min]": minDate,
-      "f[date][max]": maxDate,
-      status: Array.isArray(status) && status.length ? status.join(",") : "",
-    };
-
-    // اضافه کردن فیلتر نوع تراکنش فقط اگر "همه" انتخاب نشده باشد
-    if (transactionType !== "all") {
-      newFilters.coefficient = transactionType === "deposit" ? "1" : "-1";
-    }
-
-    // اضافه کردن فیلتر کد رهگیری فقط اگر مقدار داشته باشد
-    if (trackingCode && trackingCode.trim() !== "") {
-      newFilters.tracking_code = trackingCode.trim();
-    }
-
-    setActiveFilters(newFilters);
-  };
-
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
-      <div>
-        <div className="w-full border-b border-white/10 p-6">
-          <h1 className="text-white text-2xl font-bold">تطبیق فاکتور فروش و خرید</h1>
-          <p className="text-white/60 text-sm mt-1">
-            نمای کلی تطبیق فاکتور فروش و خرید
-          </p>
-        </div>
-      </div>
-      <div className="p-2">
-        <ReportsFilterBill
-          startDate={startDate}
-          endDate={endDate}
-          onStartDateChange={handleStartDateChange}
-          onEndDateChange={handleEndDateChange}
-          onClearAll={handleClearAll}
-          setStatus={setStatus}
-          status={status}
-          onSendAll={handleSendAll}
-          transactionType={transactionType}
-          onTransactionTypeChange={setTransactionType}
-          trackingCode={trackingCode}
-          onTrackingCodeChange={setTrackingCode}
-        />
-
-        <div className="mt-6">
-          <>
-            <BillRecordsTable
-              records={filteredData}
-              loading={loading}
-              selectedCustomerId={selectedCustomerId}
-              setSelectedCustomerId={setSelectedCustomerId}
-            />
-
-            <Pagination
-              meta={meta}
+ return (
+  <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 p-2">
+  <div>
+    <div className="w-full border-b border-white/10 p-6">
+      <h1 className="text-white text-2xl font-bold">تطبیق فاکتور فروش و خرید</h1>
+      <p className="text-white/60 text-sm mt-1">نمای کلی تطبیق فاکتور فروش و خرید</p>
+    </div>
+  </div>
+<div className="flex flex-col lg:flex-row gap-2 mt-6 px-2">
+  <button
+     className={`btn-custom6 lg:btn-custom4 ${
+            activeBtn === "invoiceAccount" ? "btn-active" : ""
+          }`}
+  onClick={() => setActiveBtn("invoiceAccount")}
+  >
+    فاکتور با فاکتور
+  </button>
+  <button
+     className={`btn-custom6 lg:btn-custom4 ${
+            activeBtn === "accountInvoice" ? "btn-active" : ""
+          }`}
+  onClick={() => setActiveBtn("accountInvoice")}
+  >
+ خرید با فروش
+  </button>
+  <button
+  className={`btn-custom6 lg:btn-custom4 ${
+            activeBtn === "all" ? "btn-active" : ""
+          }`}
+  onClick={() => setActiveBtn("all")}
+    >
+   فروش با خرید
+  </button>
+  <button
+  className={`btn-custom6 lg:btn-custom4 ${
+            activeBtn === "all2" ? "btn-active" : ""
+          }`}
+  onClick={() => setActiveBtn("all2")}
+    >
+    تعیین تکلیف شده 
+  </button>
+</div>
+      <div className="px-3">
+        {activeBtn === "invoiceAccount" && <div className="mt-6">
+          <InvoiceAccountTable
+             invoiceData={invoiceData}
+               loading={loading}
+               setRefresh={setRefresh}
+               refresh={refresh}
+               setLoading={setLoading}
+               meta3={meta3}
+               pageCount3={pageCount3}
+               setPageCount3={setPageCount3}
+                />
+      
+          </div>}
+        {activeBtn === "accountInvoice" && <div>
+          <AccountInvois
+             invoiceData={transactionData2} 
+             meta2={meta2}
               pageCount={pageCount}
               setPageCount={setPageCount}
               setLoading={setLoading}
+               loading={loading}
+                setRefresh={setRefresh}
+               refresh={refresh}
+             />
+            </div>}
+        {activeBtn === "all" && <div>
+          <AccountInvoisAll
+              invoiceData={transactionDataAll} 
+             meta2={meta2}
+              pageCount={pageCount}
+              setPageCount={setPageCount}
+              setLoading={setLoading}
+               loading={loading}
+                setRefresh={setRefresh}
+               refresh={refresh}
             />
-          </>
-
-          {isInvoiceDetailsOpen && (
-            <InvoiceDetailsModal
-              isOpen={isInvoiceDetailsOpen}
-              onClose={() => setIsInvoiceDetailsOpen(false)}
-              //  data={invoiceDetails}
+          </div>}
+          {activeBtn === "all2" && <div>
+            <FactorInvoice
+               invoiceData={factorData}
+               loading={loading}
+               setRefresh={setRefresh}
+               refresh={refresh}
+               setLoading={setLoading}
+               meta3={meta3}
+               pageCount3={pageCount3}
+               setPageCount3={setPageCount3}
             />
-          )}
-        </div>
+          </div>}
       </div>
-    </div>
-  );
+</div>
+)
 }
